073d4c9492c19b89ee0063793dcdd88415615f20
majdAlmalki <38669216+majdAlmalki@users.noreply.github.com>
2024-01-07 21:08:10 +0300

    -

--- Assets/Scripts/GoThrowPortal.cs
+++ Assets/Scripts/GoThrowPortal.cs
@@ -5,12 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class GoThrowPortal : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
